93a2f5d95f03aaac75ac64e453d354d66bfcf36b
Amandine <amandine.labaeye@orange.fr>
2018-10-09 16:56:56 +0200

    Cv version 1.0

---
+++ cv.js
@@ -0,0 +1,26 @@
+function boutonun () {
+    var x = document.getElementById('experiencepro');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function boutondeux () {
+    var x = document.getElementById('diplomeformation');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function boutontrois () {
+    var x = document.getElementById('competences');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
